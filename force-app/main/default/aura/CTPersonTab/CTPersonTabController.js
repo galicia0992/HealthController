6fcb4feb3d22de5e055e9251ab22639ddcb87809
galicia0992 <perrako92@live.com.mx>
2024-09-12 23:20:58 -0600

    featura/CTLocationTabController: se crea el apex controller que muestra una lista con locaciones y visitantes

--- force-app/main/default/aura/CTPersonTab/CTPersonTabController.js
+++ force-app/main/default/aura/CTPersonTab/CTPersonTabController.js
@@ -1,5 +1,29 @@
 ({
-    myAction : function(component, event, helper) {
-
+    userSearchHandler: function(cmp, e, helper){
+        cmp.set('v.columns', [
+            {
+                label: "Id",
+                fieldName: "Id",
+                type: "text"
+            },
+            {
+                label: "Status",
+                fieldName: "status",
+                type: "text"
+            },
+            {
+                label: "Token",
+                fieldName: "token",
+                type: "text"
+            },
+            {
+                label: "Contact Date",
+                fieldName: "contactDate",
+                type: "date"
+            }
+        ]);
+        cmp.set('v.userFound', 'true')
+        
+        helper.fetchData(cmp)
     }
 })
--- force-app/main/default/aura/CTPersonTab/CTPersonTabHelper.js
+++ force-app/main/default/aura/CTPersonTab/CTPersonTabHelper.js
@@ -1,5 +1,25 @@
 ({
-    helperMethod : function() {
+    fetchData: function (cmp) {
+        var action = cmp.get("c.getPersonDetails");
+        let recordId = cmp.get('v.recordId')
+        action.setParams({recordId});
 
+        action.setCallback(this, function (response) {
+            var state = response.getState();
+            if (state === "SUCCESS") {
+                let res = response.getReturnValue()
+                if(!res || !res.name){
+                    cmp.set('v.userFound', false)
+                    this.showToast('ERROR', 'Please enter a valid user id', 'error')
+                }else{
+                    cmp.set('v.userFound', true)
+                    cmp.set('v.userInfo', res)
+                }
+            } else {
+                alert("Error: " + response.getError());
+            }
+        });
+
+        $A.enqueueAction(action);
     }
 })
